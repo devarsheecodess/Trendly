b9596589e5bde32246327b3a17c114d943ce561f
devarsheecodess <devarsheegaunekar29@gmail.com>
2025-04-04 03:08:15 +0530

    Minor fix #3

--- Backend/routes/UploadRoute.js
+++ Backend/routes/UploadRoute.js
@@ -1,6 +1,12 @@
 const express = require("express");
-const cloudinary = require("../config/cloudinaryConfig.js");
 const upload = require("../config/multerConfig.js");
+const { v2: cloudinary } = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const router = express.Router();
 
